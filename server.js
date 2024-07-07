4f2df335bf8ccdffee4ac144b3feb3c9e13f18d1
hmdnz <hammadmcferri@gmail.com>
2024-07-07 18:29:00 +0100

    dot json commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT =process.env.PORT || 5000
 
 app.get('/api/contacts',(req, res)=>{
-res.send ("<h1> Hello fron NodeJs Server</h1>")
+res.json({"message":"Get all contacts"})
 })
 
 app.listen(PORT, () =>{
