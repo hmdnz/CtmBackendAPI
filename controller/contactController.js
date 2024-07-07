470aed1feb07fc3024735d6b12be3d2719273d04
hmdnz <hammadmcferri@gmail.com>
2024-07-07 20:19:18 +0100

    controller created

---
+++ controller/contactController.js
@@ -0,0 +1,5 @@
+//@desc get all contacts
+//@route GET api/contacts
+//@access public
+
+const getContact =
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,33 @@
-const express = require ('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Calling a route with @router.route
-router.route('/').get((req, res)=>{
-res.status(200).json({"message":"Get all contacts"})
-})
 
+// The routes for CRUD operations below:
+// GET ALL
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
 
-module.exports = router
+// GET Single/ID
+
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+});
+
+// POST
+router.route("/:id").post((req, res) => {
+  res.status(201).json({ message: `Create Contact for ${req.params.id}` });
+});
+
+// PUT
+router.route("/").put((req, res) => {
+  res.status(200).json({ message: `Update Contact for ${req.params.id}` });
+});
+
+// DELETE
+router.route("/").delete((req, res) => {
+  res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
+});
+
+module.exports = router;
