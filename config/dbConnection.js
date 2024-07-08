069a81d1fd867132f69dea5d69b88892be28bbcf
hmdnz <hammadmcferri@gmail.com>
2024-07-08 15:51:03 +0100

    mongo connect2

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,8 @@
 
  const connectionDb =async ()=>{
     try {
-        const connect=await mongoose.connect
+        const connect=await mongoose.connect(process.env.CONNETION_STRING)
+        console.log('Database Connected!!!', connection.host, connection.name)
     } catch (error) {
         console.log(error)
         process.exit(1)
